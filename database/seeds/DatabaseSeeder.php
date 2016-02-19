425734118937a34c39b19ba3e2b60b2a060bbd14
Ethan Luke Stenis <ethanstenis@gmail.com>
2016-02-18 20:13:27 -0600

    Update functional Database Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,12 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserTableSeeder::class);
-		factory(App\User::class, 50)->create()->each(function($user) {
+		factory(App\User::class, 10)->create()->each(function($user) {
 			$user->posts()->save(factory(App\Post::class)->make());
 
-			$user->posts()->save(factory(App\Post::class)->make([
-			'user_id' => rand(1,App\Post::all()->count())
-			]);
+            $post = App\Post::find(rand(1, App\Post::all()->count()));
+
+			$user->userPosts()->attach($post);
 		});
     }
 }
