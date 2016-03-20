6b1bb24f94a0af842b58018937f0af76a9d79e95
Ethan Luke Stenis <ethanstenis@gmail.com>
2016-03-20 14:14:48 -0500

    Update App.js with Models/Views/Collections

--- public/js/app.js
+++ public/js/app.js
@@ -6,33 +6,91 @@ $.ajaxSetup({
     }
 });
 
+// MODELS
+
+// This creates the Frontend Post Model
 var PostModel = Backbone.Model.extend({
 	urlRoot: '/api/posts/',
-	idAttribute: 'id',
+	idAttribute: 'id'
 });
 
+// This creates the Frontend User Model
+var UserModel = Backbone.Model.extend({
+	urlRoot: '/api/users/',
+	idAttribute: 'id'
+});
+
+// COLLECTIONS
+
+// This creates the Frontend Posts Collection
 var PostCollection = Backbone.Collection.extend({
 	url: '/api/posts/',
 	model: PostModel
 });
 
+// This creates the Frontend User Collection
+var UsersCollection = Backbone.Collection.extend({
+	url: '/api/users/',
+	model: UserModel
+});
+
+// VIEWS
+
+// This creates the Frontend Post Item View
 var PostItemView = Backbone.View.extend({
-	el:'<div></div>',
+	el: '<li class="hello"></li>',
 
 	template: _.template('<h2><%= post.get("title") %></h2>'),
 
+  events: {
+    'click h2': function(e) {
+        this.model.destroy();
+    }
+  },
+
+  initialize: function() {
+    this.listenTo(this.model, 'all', function() {
+      console.log(arguments);
+    });
+    this.listenTo(this.model, 'sync', this.render);
+  },
+
 	render: function() {
-		this.$el.html(this.template({ post: this.model }));
+		this.$el.html(this.template({post: this.model}));
 	}
 });
 
-var post = new PostModel({id: 1});
+var PostsListView = Backbone.View.extend({
+	el: '<ul></ul>',
 
-post.fetch({
-	success: function() {
-		var postItemView = new PostItemView({ model: post });
-		postItemView.render();
+	template: 'undefined',
 
-		$('#content').html(postItemView.el);
+  initialize: function() {
+    this.listenTo(this.collection, 'all', function() {
+      console.log(event);
+    });
+    this.listenTo(this.collection, 'sync update', this.render);
+  },
+
+	render: function() {
+		var that = this;
+    this.$el.html('');
+		this.collection.each(function(postModel) {
+			var postItemView = new PostItemView({ model: postModel });
+			postItemView.render();
+			that.$el.append(postItemView.el);
+		});
+    return this;
 	}
 });
+
+// New iteration of a Posts Collection
+var posts = new PostsCollection();
+// fetch data for posts
+posts.fetch();
+
+var postsListView = new PostsListView({collection: posts});
+postsListView.render();
+
+$('#content').html(postsListView.el);
+  console.log('view inserted!');
