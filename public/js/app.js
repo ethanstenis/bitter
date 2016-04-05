cc94116cb43c049832bbc0f5c4f177048e2412eb
Ethan Luke Stenis <ethanstenis@gmail.com>
2016-04-05 18:24:52 -0500

    Rendering the Posts

--- public/js/app.js
+++ public/js/app.js
@@ -5,37 +5,37 @@
 // $(document).ready(function) {
 
 $.ajaxSetup({
-    headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
+  headers: {
+    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+  }
 });
 
 // MODELS
 
 // This creates the Frontend Post Model
 var PostModel = Backbone.Model.extend({
-	urlRoot: '/api/posts/',
-	idAttribute: 'id'
+  urlRoot: '/api/posts/',
+  idAttribute: 'id'
 });
 
 // This creates the Frontend User Model
 var UserModel = Backbone.Model.extend({
-	urlRoot: '/api/users/',
-	idAttribute: 'id'
+  urlRoot: '/api/users/',
+  idAttribute: 'id'
 });
 
 // COLLECTIONS
 
 // This creates the Frontend Posts Collection
 var PostCollection = Backbone.Collection.extend({
-	url: '/api/posts/',
-	model: PostModel
+  url: '/api/posts/',
+  model: PostModel
 });
 
 // This creates the Frontend User Collection
 var UsersCollection = Backbone.Collection.extend({
-	url: '/api/users/',
-	model: UserModel
+  url: '/api/users/',
+  model: UserModel
 });
 
 // VIEWS
@@ -49,23 +49,27 @@ var HomeView = Backbone.View.extend({
         <div class ="six columns"></div>\
           <div class="row">\
             <div class="twelve columns"></div>\
-          <div>\
-          <div class="row">\
-            <div clss="twelve columns"></div>\
-            </div>\
-            <div class="twelve columns" id="all-posts"></div>\
-            </div>\
-            </div>\
+              <div>\
+              <div class="row">\
+              <div clss="twelve columns"></div>\
+          </div>\
+          </div>\
+        <div class="twelve columns" id="all-posts"></div>\
+      </div>\
+    </div>\
             ',
 
-  render: function(){
-    var that = this;
+  insertPosts: function() {
     var posts = new PostsCollection();
     posts.fetch();
     var postsListView = new PostsListView({
-        collection: posts
+      collection: posts
     });
-    this.$el.find('#all-posts').html(postsListView.render().el);
+    this.$el.find('posts').html(postsListView.render().el);
+  }
+
+    render: function() {
+    this.insertPosts();
     return this;
   }
 });
@@ -74,15 +78,15 @@ var HomeView = Backbone.View.extend({
 
 // This creates the Frontend Post Item View
 var PostItemView = Backbone.View.extend({
-	el: '<li class="hello"></li>',
+  el: '<li class="hello"></li>',
 
-	template: _.template('<h2><%= post.get("title") %></h2>'),
+  template: _.template('<h2><%= post.get("title") %></h2>'),
 
   events: {
     'click h2': function(e) {
-        this.model.destroy({
-          wait: true
-        });
+      this.model.destroy({
+        wait: true
+      });
     }
   },
 
@@ -93,15 +97,17 @@ var PostItemView = Backbone.View.extend({
     this.listenTo(this.model, 'sync', this.render);
   },
 
-	render: function() {
-		this.$el.html(this.template({post: this.model}));
-	}
+  render: function() {
+    this.$el.html(this.template({
+      post: this.model
+    }));
+  }
 });
 
 var PostsListView = Backbone.View.extend({
-	el: '<ul></ul>',
+  el: '<ul></ul>',
 
-	template: _.template('\
+  template: _.template('\
     <% posts.each(function(post)) { %>\
       <li><a href="#"><% post.get("title") %></a></li>\
       <% }) %>\
@@ -114,28 +120,34 @@ var PostsListView = Backbone.View.extend({
     this.listenTo(this.collection, 'sync update', this.render);
   },
 
-	render: function() {
-		var that = this;
+  render: function() {
+    var that = this;
     this.$el.html('');
-		this.collection.each(function(postModel) {
-			var postItemView = new PostItemView({ model: postModel });
-			postItemView.render();
-			that.$el.append(postItemView.el);
-		});
+    this.collection.each(function(postModel) {
+      var postItemView = new PostItemView({
+        model: postModel
+      });
+      postItemView.render();
+      that.$el.append(postItemView.el);
+    });
     return this;
-	}
+  }
 });
 
 var homeView = new HomeView();
 $#('#content').html(homeView.render().el);
 
+/*
 // New iteration of a Posts Collection
 var posts = new PostsCollection();
 // fetch data for posts
 posts.fetch();
 
-var postsListView = new PostsListView({collection: posts});
+var postsListView = new PostsListView({
+  collection: posts
+});
 postsListView.render();
 
 $('#content').html(postsListView.el);
-  console.log('view inserted!');
+console.log('view inserted!');
+*/
